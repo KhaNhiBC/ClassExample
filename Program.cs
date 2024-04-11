def361586f352e4414889f8a15c752e17f1f46c1
KhaNhi <KhaNhi@users.noreply.github.com>
2024-04-11 21:29:03 +0700

    add1

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Rectange rectange= new Rectange(4,5);
+        //Console.WriteLine($"Chieu rong va cao {rectange.with} {rectange.height}");
+        rectange.Show();
+        Rectange.Showrec();  
+    }
+ 
+}
+
+public class Rectange
+{
+    public static int rec=10;
+    public float with;
+    public float height;
+    public Rectange(float with, float height)
+    {
+        this.with = with;
+        this.height = height;
+    }
+    public void Show ()
+    {
+        Console.WriteLine($"Chieu rong va cao la {with} {height}" );
+    }
+    public static void Showrec()
+    {
+        Console.WriteLine($"{rec}");
+    }
+}
